36c33fb6453725d889bf92ade4c796f5d45e03a5
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-20 11:54:23 +0100

    "Base correcte"

--- src/pizza-game/index.js
+++ src/pizza-game/index.js
@@ -1,8 +1,11 @@
 import angular from 'angular';
 
 import { PizzaPool } from './pizza-pool/pizza-pool.component'
+import { PizzaGame } from './pizza-game.component'
 
 export default angular.module('PizzaGame', [])
+
+.component('pizzaGame', PizzaGame)
 
 .component('pizzaPool', PizzaPool)
 
--- src/pizza-game/pizza-game.component.js
+++ src/pizza-game/pizza-game.component.js
@@ -1,14 +1,13 @@
+import css from './pizza-game.component.css'
 class PizzaGameController {
     constructor() {
-
+        console.log('test')
     }
 }
 
 export const PizzaGame = {
     template: ` 
-       <pizza-pool>
-        sdfsdfsd
-       </pizza-pool>
+       <pizza-pool></pizza-pool>
     `,
     controller: PizzaGameController
 }
