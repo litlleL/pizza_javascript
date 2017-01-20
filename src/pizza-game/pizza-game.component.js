663fee32907201e709c67e9e013b0f4486c5915a
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-20 12:41:55 +0100

    "diplay recipies"

--- src/pizza-game/pizza-game.component.js
+++ src/pizza-game/pizza-game.component.js
@@ -4,6 +4,28 @@ class PizzaGameController {
     constructor() {
         console.log('test')
     }
+
+    $onInit() {
+        this.pizzas = [{
+                name: 'Margharita',
+                toppings: ["pate", "tomato", "ham", "fromaggio"]
+            },
+            {
+                name: 'Peperoni',
+                toppings: ["pate", "tomato", "ham", "fromaggio"]
+            },
+            {
+                name: 'Reine',
+                toppings: ["pate", "tomato", "ham", "fromaggio"]
+            }
+        ]
+    }
+
+    displayRecepicies(pizza) {
+        console.log(pizza)
+        this.recipie = pizza
+        console.log(this.recipie)
+    }
 }
 
 export const PizzaGame = {
--- src/pizza-game/pizza-pool/pizza-pool.component.js
+++ src/pizza-game/pizza-pool/pizza-pool.component.js
@@ -4,18 +4,28 @@
 
      }
 
-     $onChanges(changes) {
 
+     $onChanges(changes) {
+         this.name = changes.pizza.currentValue.name;
      }
 
-
+     recepiesSelect() {
+         this.onRecepiciesselect()
+     }
  }
 
  export const PizzaPool = {
-
+     bindings: {
+         pizza: '<',
+         onRecepiciesselect: '&'
+     },
 
      template: `
-        <div >ceci sera le pool de PizzaPool</div>
+        <div >
+            <button ng-click="$ctrl.recepiesSelect()" type="button" class="btn btn-lg " >
+                {{ $ctrl.name }}
+            </button>
+        </div>
     `,
 
      controller: PizzaPoolController
