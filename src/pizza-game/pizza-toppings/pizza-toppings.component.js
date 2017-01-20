a855707379639a3bc5fcf44efd780b980d5e26aa
joel T <joel.torib@gmail.com>
2017-01-20 15:07:31 +0100

    avancement

--- src/pizza-game/pizza-toppings/pizza-toppings.component.js
+++ src/pizza-game/pizza-toppings/pizza-toppings.component.js
@@ -1,3 +1,31 @@
+class PizzaToppingsController{
+    constructor(){
+
+    }
+    $onInit(){
+        
+        
+    }
+    $onChanges(){
+        // if (changes.listToppings && changes.listToppings.currentValue) {
+        //   this.toppings = angular.copy(changes.listToppings.currentValue);  
+        // } 
+    }
+    toppingSelect(idTopping){
+        
+    }
+}
+
 export const PizzaToppings ={
-    template : '<div>Je suis la dansx la pizza-toppings</div>'
+    bindings:{
+        listToppings:'<'
+
+    },
+    template : `  <button ng-click="$ctrl.toppingSelect()" type="button" >
+                    <span>&times;</span>
+                     </button>
+                     <pre>
+                        
+                     </pre>`,
+    controller: PizzaToppingsController
 }
