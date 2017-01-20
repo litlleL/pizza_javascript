a3529d28116187b24ce3c1d0679beb32facc14cf
joel T <joel.torib@gmail.com>
2017-01-20 14:12:50 +0100

    Commit

--- src/pizza-game/pizza-recipes/pizza-recipes.component.js
+++ src/pizza-game/pizza-recipes/pizza-recipes.component.js
@@ -1,13 +1,22 @@
-class PizzaRecipesController {
-    constructor() {}
-    $onInit() {
-        this.pizza = {
-            "name": "Margarita",
-            "toppings": ["ham", "chesses", "Ham"]
-        }
+
+class PizzaRecipesController{
+    constructor(){}
+    $onInit(){
+        //this.recipe=recipeSelect;
+    }
+
+    $onChanges(changes){
+         this.recipe= changes.recipeSelected;
+         console.log( this.recipe)
     }
 }
 
-export const PizzaRecipes = {
-    template: require('./pizza-recipes.component.html')
+export const PizzaRecipes= {
+     bindings: {
+        recipeSelected: '<'
+  
+    },
+    template: require('./pizza-recipes.component.html'),
+    controller: PizzaRecipesController
+
 }
