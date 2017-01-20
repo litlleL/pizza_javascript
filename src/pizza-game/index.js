79b794bf779b59da79c73499726ad2f038559b1f
joel T <joel.torib@gmail.com>
2017-01-20 13:42:29 +0100

    va

--- src/pizza-game/index.js
+++ src/pizza-game/index.js
@@ -2,11 +2,11 @@ import angular from 'angular';
 
 import { PizzaPool } from './pizza-pool/pizza-pool.component'
 import { PizzaGame } from './pizza-game.component'
-
+import { PizzaRecepies } from './'
 export default angular.module('PizzaGame', [])
 
 .component('pizzaGame', PizzaGame)
-
 .component('pizzaPool', PizzaPool)
+.component('pizzaRecipes',PizzaRecipes)
 
 .name;
