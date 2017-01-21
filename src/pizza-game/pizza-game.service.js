89a34efda8c24c67ddfd876cb93fd9f997c1b9c9
Boris MAURENCE <contact@boris-maurence.com>
2017-01-21 14:49:43 +0100

    dev and test of checkPizza() method

--- src/pizza-game/pizza-game.service.js
+++ src/pizza-game/pizza-game.service.js
@@ -43,19 +43,36 @@ export class PizzaService {
     }
 
     checkPizza(pizza) {
+        let recipeValid = {}
         this.allPizzas.forEach(recipe => {
+            console.log('Checking if ', recipe.toppings, 'and ', pizza, 'are egals')
             if (this.isRecipeCompliant(recipe, pizza)) {
-                return recipe
+                recipeValid = recipe
             }
         })
+        return recipeValid
     }
 
     isRecipeCompliant(recipe, pizza) {
         if (pizza.length !== recipe.toppings.length) {
             return false
+        } else {
+            let pizzaToppings = angular.copy(pizza)
+            let recipeToppings = angular.copy(recipe.toppings)
+            pizza.forEach(topping => {
+                console.log('checking ', topping)
+                if(recipe.toppings.includes(topping)) {
+                    console.log('topping deleted : ', topping)
+                    recipeToppings.splice(topping, 1)
+                    pizzaToppings.splice(topping, 1)
+                }
+            })
+            return (pizzaToppings.length <= 0 && recipeToppings.length <= 0)
+            // return pizza.reduce((boolValue, topping) => {
+            // boolValue && recipe.toppings.indexOf(topping) !== -1 && pizza.indexOf(topping) === pizza.lastIndexOf(topping)
+            // console.log('topping : ', topping)
+            // console.log('boolValue : ', boolValue)
+            // }, true)
         }
-        return pizza.reduce((boolValue, topping) => boolValue &&
-            recipe.toppings.indexOf(topping) !== -1 &&
-            pizza.indexOf(topping) === pizza.lastIndexOf(topping), true)
     }
 }
