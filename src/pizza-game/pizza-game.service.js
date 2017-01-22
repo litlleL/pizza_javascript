aed3cd1d1e9a59b210e40fd4ca747e364eb3abd5
litlleL@litlleL-Pc <litlleL@litlleL-Pc>
2017-01-22 16:21:21 +0100

    ajout

--- src/pizza-game/pizza-game.service.js
+++ src/pizza-game/pizza-game.service.js
@@ -26,7 +26,7 @@ export class PizzaService {
                 "toppings": ["pate", "mayo", "eeg", "onions", "creme fraiche"]
             }
         ]
-        this.allToppings = ["mayo", "eeg", "onions", "creme fraiche", "pate", "tomato", "ham", "fromaggio", "parmeggiano", "cheese1", "cheeese 2", "beets", "potato"]
+        this.allToppings = ["mayo", "eeg", "mushrooms", "onions", "creme fraiche", "pate", "tomato", "ham", "fromaggio", "parmeggiano", "cheese1", "cheeese 2", "beets", "potato"]
     }
 
     getAllRecipes() {
